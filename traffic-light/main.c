841d3b230035a29e6532a6d16a8a6246a3ba4a8b
ahnafi <devsulthon@gmail.com>
2026-03-18 13:51:36 +0700

    add (traffic-light/main.c): menambah comentar documentasi kode

--- traffic-light/main.c
+++ traffic-light/main.c
@@ -1,5 +1,9 @@
 // code traffic light tinkercad
 
+/*
+* inisialisasi constanta lampu dari utara -> barat -> selatan -> timur
+* dengan pin digital mulai dari 13 sampai 2
+*/
 // north
 const int RNORTH = 13;
 const int YNORTH = 12;
@@ -20,6 +24,15 @@ const int REAST = 4;
 const int YEAST = 3;
 const int GEAST = 2;
 
+/*
+* fungsi untuk menyalakan traffic light pada satu sisi.
+* fungsi ini menyalakan semua lampu traffic light merah,
+* selanjutnya untuk menyalakan lampu disisi tersebut.
+* void light(int redLight,int yellowLight, int greenLight)
+* int redLight untuk pin led warna merah,
+* int yellowLight untuk pin led warna kuning
+* int greenLight untuk pin led warna hijau
+*/
 void light(int red, int yellow, int green){
   digitalWrite(REAST, HIGH);
   digitalWrite(RSOUTH, HIGH);
@@ -41,6 +54,10 @@ void light(int red, int yellow, int green){
   digitalWrite(red, HIGH);
 }
 
+/*
+* void setup() disini digunakan sebagai inisialisasi pin pada arduino
+* pinMode pada semua lampu led sebagai output
+*/
 void setup(){
   pinMode(RNORTH, OUTPUT);
   pinMode(YNORTH, OUTPUT);
@@ -59,6 +76,10 @@ void setup(){
   pinMode(GEAST, OUTPUT);
 }
 
+/*
+* void loop() menyalakan lampu traffic light berurutan dari arah utara -> barat -> selatan -> timur
+* dan kembali ke arah utara lagi 
+*/
 void loop(){
     // north
     light(RNORTH, YNORTH, GNORTH);
@@ -71,5 +92,5 @@ void loop(){
 
     // east
     light(REAST, YEAST, GEAST);
-
 }
+
