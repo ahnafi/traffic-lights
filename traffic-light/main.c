a27bd3d22acf30ea86ef537e2032636cdbdb8652
ahnafi <devsulthon@gmail.com>
2026-03-20 10:22:08 +0700

    fix : arah rotasi traffic light searah jarum jam

--- traffic-light/main.c
+++ traffic-light/main.c
@@ -1,7 +1,7 @@
 // code traffic light tinkercad
 
 /*
-* inisialisasi constanta lampu dari utara -> barat -> selatan -> timur
+* inisialisasi constanta lampu dari utara -> timur -> selatan -> barat
 * dengan pin digital mulai dari 13 sampai 2
 */
 // north
@@ -9,20 +9,20 @@ const int RNORTH = 13;
 const int YNORTH = 12;
 const int GNORTH = 11;
 
-// west
-const int RWEST = 10;
-const int YWEST = 9;
-const int GWEST = 8;
+// east
+const int REAST = 10;
+const int YEAST = 9;
+const int GEAST = 8;
 
 // south
 const int RSOUTH = 7;
 const int YSOUTH = 6;
 const int GSOUTH = 5;
 
-// east
-const int REAST = 4;
-const int YEAST = 3;
-const int GEAST = 2;
+// west
+const int RWEST = 4;
+const int YWEST = 3;
+const int GWEST = 2;
 
 /*
 * fungsi untuk menyalakan traffic light pada satu sisi.
@@ -34,46 +34,68 @@ const int GEAST = 2;
 * int greenLight untuk pin led warna hijau
 */
 void light(int red, int yellow, int green){
+  // menyalakan semua lampu merah
   digitalWrite(REAST, HIGH);
   digitalWrite(RSOUTH, HIGH);
   digitalWrite(RWEST, HIGH);
   digitalWrite(RNORTH, HIGH);
-  delay(3000);
+
+  // mematikan lampu merah pada simpang 
+  delay(1000)
   digitalWrite(red, LOW);
 
-  for (int i=0;i<3;i++){
+  // Menyalakan lampu kuning 
+  // berkedip sebanyak 3x dalam 2 detik
+  for (int i = 0; i < 3; i++) {
    digitalWrite(yellow, HIGH);
-   delay(500);
+   delay(333); // Lampu menyala selama 333 ms
    digitalWrite(yellow, LOW);
-   delay(500);
+   delay(333); // Lampu mati selama 333 ms
   }
 
+  //menyalakan lampu hijau selama 5 detik
   digitalWrite(green, HIGH);
-  delay(3000);
+  delay(5000);
   digitalWrite(green, LOW);
+
+  // menyalakan lampu merah
   digitalWrite(red, HIGH);
 }
 
 /*
 * void setup() disini digunakan sebagai inisialisasi pin pada arduino
-* pinMode pada semua lampu led sebagai output
+* pinMode pada semua lampu led merah, kuning, dan hijau sebagai output
+* mulai dari arah utara -> timur -> selatan -> barat (sesuai arah jam)
+* selanjutnya inisialisasi untuk menyalakan semua lampu merah agar ketika dinyalakan 
+* pertama kali lampu merah sudah menyala
 */
 void setup(){
+  // north led
   pinMode(RNORTH, OUTPUT);
   pinMode(YNORTH, OUTPUT);
   pinMode(GNORTH, OUTPUT);
 
-  pinMode(RWEST, OUTPUT);
-  pinMode(YWEST, OUTPUT);
-  pinMode(GWEST, OUTPUT);
+  // east led
+  pinMode(REAST, OUTPUT);
+  pinMode(YEAST, OUTPUT);
+  pinMode(GEAST, OUTPUT);
 
+  // south led
   pinMode(RSOUTH, OUTPUT);
   pinMode(YSOUTH, OUTPUT);
   pinMode(GSOUTH, OUTPUT);
 
-  pinMode(REAST, OUTPUT);
-  pinMode(YEAST, OUTPUT);
-  pinMode(GEAST, OUTPUT);
+  // west led
+  pinMode(RWEST, OUTPUT);
+  pinMode(YWEST, OUTPUT);
+  pinMode(GWEST, OUTPUT);
+
+  // menyalakan semua lampu merah
+  digitalWrite(REAST, HIGH);
+  digitalWrite(RSOUTH, HIGH);
+  digitalWrite(RWEST, HIGH);
+  digitalWrite(RNORTH, HIGH);
+
 }
 
 /*
@@ -84,13 +106,14 @@ void loop(){
     // north
     light(RNORTH, YNORTH, GNORTH);
 
-    // west
-    light(RWEST, YWEST, GWEST);
+    // east
+    light(REAST, YEAST, GEAST);
 
     // south
     light(RSOUTH, YSOUTH, GSOUTH);
 
-    // east
-    light(REAST, YEAST, GEAST);
+    // west
+    light(RWEST, YWEST, GWEST);
+
 }
 
